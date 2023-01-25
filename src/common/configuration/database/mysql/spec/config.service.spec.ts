e5188b9dfaea38846b8c3bf1b664d3bdab2a5a2d
pythonstrup <pythonstrup@gmail.com>
2023-01-25 11:21:28 +0900

    feat: MySQL Config Test

---
+++ src/common/configuration/database/mysql/spec/config.service.spec.ts
@@ -0,0 +1,58 @@
+import { MysqlConfigService } from '@config/database/mysql/config.service';
+import { Test } from '@nestjs/testing';
+import { MysqlConfigModule } from '@config/database/mysql/config.module';
+
+describe('MysqlConfigService Test', () => {
+  let mysqlConfigService: MysqlConfigService;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      imports: [MysqlConfigModule],
+    }).compile();
+
+    mysqlConfigService = module.get(MysqlConfigService);
+  });
+
+  test('MYSQL_PORT를 잘가져오는가', async () => {
+    // given
+    const portNumber = parseInt(process.env.MYSQL_PORT, 10);
+    // when
+
+    // then
+    expect(mysqlConfigService.port).toEqual(portNumber);
+  });
+
+  test('MYSQL_HOST를 잘가져오는가', async () => {
+    // given
+    const hostname = process.env.MYSQL_HOST;
+
+    // when
+    // then
+    expect(mysqlConfigService.hostname).toEqual(hostname);
+  });
+
+  test('MYSQL_USER를 잘가져오는가', async () => {
+    // given
+    const username = process.env.MYSQL_USER;
+
+    // when
+    // then
+    expect(mysqlConfigService.username).toEqual(username);
+  });
+  test('MYSQL_PASSWORD를 잘가져오는가', async () => {
+    // given
+    const password = process.env.MYSQL_PASSWORD;
+
+    // when
+    // then
+    expect(mysqlConfigService.password).toEqual(password);
+  });
+  test('MYSQL_DATABASE를 잘가져오는가', async () => {
+    // given
+    const database = process.env.MYSQL_DATABASE;
+
+    // when
+    // then
+    expect(mysqlConfigService.database).toEqual(database);
+  });
+});
