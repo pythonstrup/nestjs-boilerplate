2246f8155136cac2e34e034f77ec4799ca68c5e7
pythonstrup <pythonstrup@gmail.com>
2023-01-25 11:22:33 +0900

    feat: TypeORM Config 작성 및 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from '@src/app.controller';
 import { AppService } from '@src/app.service';
 import { AppConfigModule } from '@config/app/config.module';
+import { DatabaseModule } from '@config/database/database.module';
 
 @Module({
-  imports: [AppConfigModule],
+  imports: [AppConfigModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
