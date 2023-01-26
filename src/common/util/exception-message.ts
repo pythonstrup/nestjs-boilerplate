00b6d06a64f41e1d8ec718a9e248ee7d8d00a5a6
pythonstrup <pythonstrup@gmail.com>
2023-01-26 19:28:33 +0900

    feat: Exception Status & Message

---
+++ src/common/util/exception-message.ts
@@ -0,0 +1,4 @@
+export enum ExceptionMessage {
+  API_NOT_FOUND = '해당하는 API가 존재하지 않습니다.',
+  BAD_PARAMETER = '입력값이 올바르지 않습니다',
+}
---
+++ src/common/util/exception-status.ts
@@ -0,0 +1,4 @@
+export enum ExceptionStatus {
+  API_NOT_FOUND = 'API_NOT_FOUND',
+  BAD_PARAMETER = 'BAD_PARAMETER',
+}
