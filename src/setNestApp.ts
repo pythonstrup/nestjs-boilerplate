b9ba1f81a4c4dbeeb34afa253a24ac711c018fea
pythonstrup <pythonstrup@gmail.com>
2023-01-26 20:32:03 +0900

    feat: Cors & Filter 설정

--- src/setNestApp.ts
+++ src/setNestApp.ts
@@ -1,6 +1,15 @@
 import { INestApplication } from '@nestjs/common';
 import { AppConfigService } from '@config/app/config.service';
+import { AllExceptionFilter } from '@filter/all-exception.filter';
+import { HttpAdapterHost } from '@nestjs/core';
 
 export const setNestApp = (app: INestApplication) => {
   const appConfigService = app.get(AppConfigService);
+
+  app.enableCors({
+    origin: appConfigService.isProduction() ? 'https://www.example.com' : true,
+    credentials: true,
+  });
+
+  app.useGlobalFilters(new AllExceptionFilter(app.get(HttpAdapterHost)));
 };
