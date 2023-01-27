654ebaa65f3e2a04b5b22ae745a72f807cf76522
pythonstrup <pythonstrup@gmail.com>
2023-01-27 17:15:55 +0900

    chore: JwtConfigService 작성

---
+++ src/common/configuration/jwt/config.service.ts
@@ -0,0 +1,23 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { jwtConfig } from '@config/jwt/configuration';
+import { ConfigType } from '@nestjs/config';
+
+@Injectable()
+export class JwtConfigService {
+  constructor(
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+  ) {}
+
+  get secret() {
+    return this.jwtConfiguration.JWT_SECRET;
+  }
+
+  get accessTokenExpirationMinutes() {
+    return this.jwtConfiguration.JWT_ACCESS_TOKEN_EXPIRATION_MINUTES;
+  }
+
+  get refreshTokenExpirationDays() {
+    return this.jwtConfiguration.JWT_REFRESH_TOKEN_EXPIRATION_DAYS;
+  }
+}
