fd3331999060cd587935a2ce8bf061199d5028cc
pythonstrup <pythonstrup@gmail.com>
2023-01-27 15:17:08 +0900

    feat: CommonEntity 작성

---
+++ src/common/entity/common.entity.ts
@@ -0,0 +1,12 @@
+import { Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+export class CommonEntity {
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt: Date;
+
+  @Column({ type: 'timestamp', nullable: true })
+  deletedAt: Date | null;
+}
