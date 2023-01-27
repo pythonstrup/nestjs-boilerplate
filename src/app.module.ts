7cbe10a0f3e7f3358540d053785eae454362c38e
pythonstrup <pythonstrup@gmail.com>
2023-01-27 15:17:44 +0900

    feat: User Module 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { DatabaseModule } from '@config/database/database.module';
 import { ApiSuccessLoggerMiddleware } from '@middleware/api-success-logger.middleware';
 import { AppConfigService } from '@config/app/config.service';
 import { ApiExceptionLoggerMiddleware } from '@middleware/api-exception-logger.middleware';
+import { UserModule } from '@domain/user/user.module';
 
 @Module({
-  imports: [AppConfigModule, DatabaseModule],
+  imports: [AppConfigModule, DatabaseModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
