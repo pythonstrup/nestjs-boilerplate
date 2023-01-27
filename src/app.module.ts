296309743ebfdaefaba5a5eefa67b8f1651a47ff
pythonstrup <pythonstrup@gmail.com>
2023-01-27 12:46:16 +0900

    feat: Middleware 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,25 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from '@src/app.controller';
 import { AppService } from '@src/app.service';
 import { AppConfigModule } from '@config/app/config.module';
 import { DatabaseModule } from '@config/database/database.module';
+import { ApiSuccessLoggerMiddleware } from '@middleware/api-success-logger.middleware';
+import { AppConfigService } from '@config/app/config.service';
+import { ApiExceptionLoggerMiddleware } from '@middleware/api-exception-logger.middleware';
 
 @Module({
   imports: [AppConfigModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  constructor(private readonly appConfigService: AppConfigService) {}
+
+  configure(consumer: MiddlewareConsumer) {
+    if (!this.appConfigService.isTest()) {
+      consumer
+        .apply(ApiSuccessLoggerMiddleware, ApiExceptionLoggerMiddleware)
+        .forRoutes('*');
+    }
+  }
+}
