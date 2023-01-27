450d1f71b02c12b3de0ab5ec1dccc87afa8884a4
pythonstrup <pythonstrup@gmail.com>
2023-01-27 12:46:47 +0900

    feat: All Exception Filter e2e Test - Internal Server Error

--- test/common/filter/all-exception.filter.e2e-spec.ts
+++ test/common/filter/all-exception.filter.e2e-spec.ts
@@ -1,4 +1,10 @@
-import { HttpStatus, INestApplication } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  INestApplication,
+} from '@nestjs/common';
 import { AppModule } from '@src/app.module';
 import { setNestApp } from '@src/setNestApp';
 import { Test } from '@nestjs/testing';
@@ -6,12 +12,24 @@ import * as request from 'supertest';
 import { ExceptionMessage } from '@common/util/exception-message';
 import { ExceptionStatus } from '@common/util/exception-status';
 
+@Controller('/test')
+class TestController {
+  @Get('/internal-server-error')
+  public testInternalServerError() {
+    throw new HttpException(
+      { message: ExceptionStatus.INTERNAL_SERVER_ERROR },
+      HttpStatus.INTERNAL_SERVER_ERROR,
+    );
+  }
+}
+
 describe('All Exception Filter e2e Test', () => {
   let app: INestApplication;
 
   beforeAll(async () => {
     const module = await Test.createTestingModule({
       imports: [AppModule],
+      controllers: [TestController],
     }).compile();
 
     app = module.createNestApplication();
@@ -20,18 +38,36 @@ describe('All Exception Filter e2e Test', () => {
   });
 
   describe('API NOT FOUND Exception', () => {
-    const url = () => `/api-not-found`;
+    const url = () => `/test/api-not-found`;
 
     test('없는 API를 요청하면 API NOT FOUND Exception을 반환한다.', async () => {
       // given
 
       // when
-      const result = await request(app.getHttpServer()).post(url());
+      const result = await request(app.getHttpServer()).get(url());
 
       // then
       expect(result.status).toEqual(HttpStatus.NOT_FOUND);
       expect(result.body.message).toEqual(ExceptionMessage.API_NOT_FOUND);
       expect(result.body.status).toEqual(ExceptionStatus.API_NOT_FOUND);
+    });
+  });
+
+  describe('Internal Server Error Exception', () => {
+    const url = () => `/test/internal-server-error`;
+
+    test('알 수 없는 HttpException이 반환되면 Internal Server Error Exception을 반환한다.', async () => {
+      // given
+
+      // when
+      const result = await request(app.getHttpServer()).get(url());
+
+      // then
+      expect(result.status).toEqual(HttpStatus.INTERNAL_SERVER_ERROR);
+      expect(result.body.message).toEqual(
+        ExceptionStatus.INTERNAL_SERVER_ERROR,
+      );
+      expect(result.body.status).toEqual(ExceptionStatus.INTERNAL_SERVER_ERROR);
     });
   });
 });
