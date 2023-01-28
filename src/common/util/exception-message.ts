1ca0d3ca4927e3e9a213a5740245b84a1fd1aba8
pythonstrup <pythonstrup@gmail.com>
2023-01-28 16:28:09 +0900

    feat: exception 메시지 추가

--- src/common/util/exception-message.ts
+++ src/common/util/exception-message.ts
@@ -1,4 +1,11 @@
 export enum ExceptionMessage {
   API_NOT_FOUND = '해당하는 API가 존재하지 않습니다.',
   BAD_PARAMETER = '입력값이 올바르지 않습니다',
+  USER_NOT_FOUND = '해당 유저가 존재하지 않습니다.',
+  INVALID_PASSWORD = '비밀번호가 틀렸습니다.',
+  DUPLICATED_USERNAME = '유저 이름이 중복되었습니다.',
+  NO_TOKEN = '토큰이 전송되지 않았습니다.',
+  INVALID_TOKEN = '토큰이 유효하지 않습니다.',
+  EXPIRED_TOKEN = '토큰이 만료되었습니다.',
+  EXPIRED_REFRESH_TOKEN = '리프레시 토큰이 만료되었습니다. 다시 로그인 해주세요.',
 }
