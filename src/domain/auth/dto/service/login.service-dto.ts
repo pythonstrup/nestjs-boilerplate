a13c9067e12b6185de3d6c76be8d39dc6f9a283a
pythonstrup <pythonstrup@gmail.com>
2023-01-29 17:21:28 +0900

    feat: LoginServiceDto of() method 추가

--- src/domain/auth/dto/service/login.service-dto.ts
+++ src/domain/auth/dto/service/login.service-dto.ts
@@ -1,4 +1,13 @@
+import { User } from '@domain/user/entity/user.entity';
+
 export class LoginServiceDto {
   id: number;
   username: string;
+
+  static of(user: User) {
+    const dto = new LoginServiceDto();
+    dto.id = user.id;
+    dto.username = user.username;
+    return dto;
+  }
 }
