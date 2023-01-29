88328bdc8e03847c5f4f1ffca702dba8d991676d
pythonstrup <pythonstrup@gmail.com>
2023-01-29 17:20:32 +0900

    feat: Exception Message & Status 작성

--- src/common/util/exception-message.ts
+++ src/common/util/exception-message.ts
@@ -8,4 +8,7 @@ export enum ExceptionMessage {
   INVALID_TOKEN = '토큰이 유효하지 않습니다.',
   EXPIRED_TOKEN = '토큰이 만료되었습니다.',
   EXPIRED_REFRESH_TOKEN = '리프레시 토큰이 만료되었습니다. 다시 로그인 해주세요.',
+  EXISTED_TOKEN = '이미 유효한 토큰이 존재합니다.',
+  INVALID_REFRESH_TOKEN = '리프레시 토큰이 유효하지 않습니다.',
+  INVALID_ACCESS = '유저 권한이 없습니다. 다시 로그인 해주세요.',
 }
